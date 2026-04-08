41553943a58e245d0b4ee02a4aa7e4ba679be92b
pinjatuulia <pinja.tahvanainen@gmail.com>
2026-04-08 15:41:13 +0300

    muutokset 8.4.

--- Harjoitustyo/app/src/main/java/main/dataakansalle/MainActivity.java
+++ Harjoitustyo/app/src/main/java/main/dataakansalle/MainActivity.java
@@ -3,6 +3,9 @@ package main.dataakansalle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,12 +13,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText SearchMunicipality;
+    TextView ErrorTextView;
+    String MunicipalityInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        SearchMunicipality = findViewById(R.id.searchMunicipality);
+        ErrorTextView = findViewById(R.id.ErrorTextView);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -23,9 +31,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
     public void switchToInformation(View view) {
-        Intent intent = new Intent(this, MunicipalityInfoActivity.class);
-        startActivity(intent);
+        MunicipalityInput = SearchMunicipality.getText().toString().trim().toUpperCase();
+        MunicipalityChecker reader = new MunicipalityChecker(this);
+        String result = reader.readLog(MunicipalityInput);
+        if (result.equals("Kunta löytyy")) {
+            Intent intent = new Intent(this, MunicipalityInfoActivity.class);
+            startActivity(intent);
+        }
+        else {
+            ErrorTextView.setText("Kuntaa ei löydy, yritä uudestaan!");
+        }
+
+
     }
 }
